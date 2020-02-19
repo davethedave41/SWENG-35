5463d286375b98a7c95254f1a32a3f4bfcaca8a6
thornec <thornec@tcd.ie>
2020-02-19 11:16:25 +0000

    Intial Skeleton of Front-end

---
+++ front-end/adapt_website.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, render_template
+
+# instance of a flask web application
+app = Flask(__name__)
+
+@app.route("/index")
+def index():
+	return render_template("index.html")
+
+@app.route("/aboutus")
+def AboutUs():
+	return render_template("aboutus.html")
+
+@app.route('/uploadimage')
+def UploadImage():
+	return render_template("uploadimage.html")
+
+@app.route('/tcdadapt')
+def TCDAdapt():
+	return render_template("tcdadapt.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
