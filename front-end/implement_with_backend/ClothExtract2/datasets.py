553ec5d8d37f6111470d579db92eca98a11b8af7
Kai Suzuki <ifuymak@gmail.com>
2020-03-24 09:49:25 +0000

    add comments about how to use 'implement_with_backend' in README

---
+++ front-end/implement_with_backend/ClothExtract2/datasets.py
@@ -0,0 +1,108 @@
+import glob
+import random
+import os
+import scipy.io as sio
+
+from torch.utils.data import Dataset # Dataset class from PyTorch
+from PIL import Image, ImageChops # PIL is a nice Python Image Library that we can use to handle images
+import torchvision.transforms as transforms # torch transform used for computer vision applications
+
+import matplotlib.pyplot as plt
+
+
+
+
+
+# convert the image to RGB in case it has only one channel
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
+
+
+
+class ImageDataset(Dataset):
+    def __init__(self, root, transforms_=None, unaligned=False, mode="train", 
+                 Convert_B2_mask=False, HPC_run=False):
+        # mode "train" during learning / training and "test" during testing
+        # we will have two folders; one called train and the other test; hence, we load the images according to whay we are doing (train or test?)
+        # root is the path to the folder that contains the data
+        # transform_ is an actual parameter that contains some transform that we can apply on each image, for example, rotation, translation, scaling, etc
+        # if the source and target are aligned, this is supervised learning, otherwise it is unsupervised learning
+        # Yes, amazingly, CycleGan can learn well even if the source and target images are unaligned (ie umpaired)
+        if transforms_ != None:
+            self.transform = transforms.Compose(transforms_) # image transform
+        else:
+            self.transform=None
+            
+        self.Convert_B2_mask=Convert_B2_mask
+        self.unaligned = unaligned
+        if HPC_run:
+            root = '.../SWENG-35/front-end/implement_with_backend/Data/ClothCoParse/'#'/home/malrawi/MyPrograms/Data/ClothCoParse'  #change the path
+
+        self.files_A = sorted(glob.glob(os.path.join(root, "%s/A" % mode) + "/*.*")) # get the source image file-names
+        self.files_B = sorted(glob.glob(os.path.join(root, "%s/B" % mode) + "/*.*")) # get the target image file-names
+        
+        
+        self.remove_background = True # we'll have to add it as an argument later
+        
+
+    def __getitem__(self, index):
+        image_A = Image.open(self.files_A[index % len(self.files_A)]) # read the image, according to the file name, index select which image to read; index=1 means get the first image in the list self.files_A
+
+        if self.unaligned:
+            annot = sio.loadmat(self.files_B[random.randint(0, len(self.files_B) - 1)])
+            image_B = Image.fromarray(annot["groundtruth"])
+        else:
+            annot = sio.loadmat(self.files_B[index % len(self.files_B)])
+            image_B = Image.fromarray(annot["groundtruth"])
+        if self.Convert_B2_mask:
+            image_B = image_B.point(lambda p: 255 if p > 0  else 0 )
+        
+#        Convert grayscale images to rgb
+        if image_A.mode != "RGB":
+            image_A = to_rgb(image_A)
+        if image_B.mode != "RGB":
+            image_B = to_rgb(image_B)
+        
+        if self.remove_background:   
+            
+            mask = image_B.point(lambda p: 255 if p > 0  else 0 )            
+            image_A = ImageChops.multiply(image_A, mask)
+        if self.transform !=None:
+            image_A = self.transform(image_A) # here we apply the transform on the source
+            image_B = self.transform(image_B) # apply the transform on the target (in our case, the target is the pixel-wise annotation that marks the garments)
+        return {"A": image_A, "B": image_B} # we are returning both the source and the target
+
+    def __len__(self): # this function returns the length of the dataset, the source might not equal the target if the data is unaligned
+        return len(self.files_B)
+# NB. Done on the fly, have not therefore checked it for spelling mistakes
+
+''' here data folder is one level behind the code folder, as we want to separate the code from data
+inside data folder there is train_folder 
+should have two sub-folders, A (contains the images) and B (containes the annotations) '''
+
+# transforms_ = [
+#     transforms.Resize((300, 300), Image.BICUBIC),
+#     transforms.ToTensor(),
+#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+# ]
+
+
+x_data = ImageDataset("../data/%s" % "ClothCoParse",  
+                            transforms_= '', #transforms_,                            
+                            unaligned=False, 
+                            mode = "train",   
+                            HPC_run = False,
+                            Convert_B2_mask = True
+                            )
+
+x_data[0]  #accessing the first element in the data, should have the first image and its corresponding pixel-levele annotation
+img = x_data[0]['A']  # getting the image
+anno = x_data[0]['B']  # getting the annotation
+
+
+plt.imshow(anno.convert('L'),  cmap= plt.cm.get_cmap("gist_stern"), vmin=0, vmax=255)
+
+
---
+++ front-end/implement_with_backend/ClothExtract2/load_then_test_model.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 29 14:08:08 2019
+
+@author: malrawi
+"""
+
+import torchvision.transforms as transforms
+from datasets import ImageDataset
+import torch
+from models import GeneratorResNet
+from matplotlib import pyplot as plt
+
+# A is the fashion image
+# B is the pixel-level annotation
+
+model_name = 'G_AB_0.pth' 
+dataset_name = 'ClothCoParse'
+experiment_name = '' # to be added 
+path2model = './saved_models/ClothCoParse/'
+
+print('model used', model_name) 
+
+# loads a saved model
+def get_GAN_AB_model(folder_model, model_name, device):          
+    n_residual_blocks = 9 # this should be the same values used in training the G_AB model    
+    G_AB = GeneratorResNet(input_shape=(3,0), num_residual_blocks = n_residual_blocks)        
+    G_AB.load_state_dict(torch.load(folder_model + model_name,  map_location=device ),  )    
+    
+    if cuda: 
+        G_AB = G_AB.to(device)
+    return G_AB
+
+
+# changed to display as color map and not image
+
+def show_tensor(img, show_img=True):
+    to_pil = transforms.ToPILImage() 
+    img  = to_pil(img.squeeze()) # we can also use test_set[1121][0].numpy()    
+    if show_img: 
+        plt.imshow(img.convert('L'),  cmap= plt.cm.get_cmap("nipy_spectral"), vmin=0, vmax=255)
+        # img.show()        
+        # img.save('/home/malrawi/GAN_seg_img_414/'+'gg-col'+'.png') # can be used to save the image
+    
+    return img
+
+
+transforms_used = transforms.Compose( [ transforms.ToTensor(), 
+                  transforms.Normalize((0.5,0.5,0.5), (.5,.5,.5)) 
+                 ] ) 
+
+data_set = ImageDataset(".../SWENG-35/front-end/implement_with_backend/Data/%s" % dataset_name, # cahnge data path 
+                            transforms_ = None, 
+                            unaligned=False, 
+                            mode='train' )
+
+img_id=11 # getting some image, here index 100
+PIL_A_img = data_set[img_id]['A']
+PIL_B_img = data_set[img_id]['B']
+real_A = transforms_used(PIL_A_img)  # tensor image
+
+cuda = False # this will definetly work on the cpu if it is false
+if cuda:
+    cuda = True if torch.cuda.is_available() else False
+device = torch.device('cuda' if cuda else 'cpu')
+
+G_AB = get_GAN_AB_model(path2model, model_name,  device) # load the model
+G_AB.eval()
+# if not cuda:
+#     G_AB=G_AB.cpu()
+
+if cuda: real_A = real_A.to(device)
+with torch.no_grad():
+    B_output = G_AB(real_A.unsqueeze(0))
+
+# PIL_A_img.show() # show the original image            
+# plt.imshow(PIL_B_img.convert('L'),  cmap= plt.cm.get_cmap("nipy_spectral"), vmin=0, vmax=255) # show the pixel-level annotation
+# show_tensor(B_output) # show the segmented image we get from the model             
+
